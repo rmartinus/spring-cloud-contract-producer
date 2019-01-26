9b85a34a86c3e68b223d3b91aa7a66d6a80cdf88
Robbie Martinus <12573669+rmartinus@users.noreply.github.com>
2019-01-26 22:20:00 +1100

    Fixed bug with table name

--- src/main/java/com/example/spring/cloud/contract/producer/MovieEntity.java
+++ src/main/java/com/example/spring/cloud/contract/producer/MovieEntity.java
@@ -4,19 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
-@Table
+@Table(name = "movie")
 public class MovieEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String genre;
--- src/test/resources/data-h2.sql
+++ src/test/resources/data-h2.sql
@@ -1,4 +1,1 @@
 INSERT INTO movie(id, name, genre, year) VALUES (1, 'My Movie', 'Action', '2019');
-INSERT INTO movie(id, name, genre, year) VALUES (2, 'My Movie 2', 'Action', '2019');
-INSERT INTO movie(id, name, genre, year) VALUES (3, 'My Movie 3', 'Action', '2019');
-INSERT INTO movie(id, name, genre, year) VALUES (4, 'My Movie 4', 'Action', '2019');
